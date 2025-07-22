657402c9b830f467c6fad2c390271c84452e476b
Danica Sugic <danica.sugic@stfc.ac.uk>
2025-07-22 15:25:51 +0100

    tidy alignment class

--- src/alignment/Alignment.py
+++ src/alignment/Alignment.py
@@ -1,49 +1,35 @@
+# author: Oriol
+# Danica only wrote the classes and tidied code
+#--------------------------------------------------
+
 import numpy as np
 import matplotlib.pyplot as plt
 import utilities.utils_tomo as utils
 from scipy.ndimage import gaussian_filter1d, measurements
 from scipy import optimize, signal
 from skimage.registration import phase_cross_correlation as register_translation
-from CT_reconstruction.TomoRecon import get_volume
 
 
-def sin_func(x, a, w, b, c):
-    return a * np.sin(w*x + b) + c
-
-
-
-
-
-def calculate_com_projs(projections):
-    #plt.figure()
+class BaseClassHorizontalAlignment():
+    """
+    This class should (will) contain basic methods useful to all alignment procedures.
+    The way the shifts are calculated is, on the other hand, determined in the specific class.
+    """
     
-    projections -= np.amin(projections)
-    summed = np.sum(projections[:,:,:],1)
-    
-    com = np.zeros([2,projections.shape[0]])
-    shifts = np.zeros([2,projections.shape[0]])
-    for i in range(projections.shape[0]):
-        com[:,i] = measurements.center_of_mass(summed[i:i+1,:])
-    
-    return com
-
-def calculate_com_projs_oriol(projections):
-    #plt.figure()
-    
-    projections -= np.amin(projections)
-    summed = np.sum(projections[:,:,:],1)
-    
-    com = np.zeros([2,projections.shape[0]])
-    shifts = np.zeros([2,projections.shape[0]])
-    for i in range(projections.shape[0]):
-        com[:,i] = measurements.center_of_mass((summed[i:i+1,:]))
-    
-    return com
-
-class BaseClassAligment():
-
-    # This function uses the shifts calculated before to "move" the projections
     def apply_reprojection_shifts(self, projections_in, shifts, pad=0):
+        """
+        This method uses the shifts calculated before to "move" the projections
+        
+        Parameters
+        ----------
+        projections_in : 3D image to be aligned
+        shifts :
+        pad :
+
+        Returns
+        -------
+        projections out : aligned projections
+        """
         projections_out = np.zeros_like(projections_in)
         projections_pad = np.copy(utils.pad(projections_in, (pad,0)))
         shifts = np.round(shifts).astype(np.int32)
@@ -87,16 +73,45 @@ class BaseClassAligment():
         
         return projections_out
 
-class VerticalAlignment():
+
+class VerticalAlignmentCrossCorrelation():
     def __init__(self, projections):
+        """Given projections, calculated the vertical alignment and applies it to them.
+        It stores the result in an attribute."""
         self.projections_aligned = self.projection_align_vertical(projections)
 
     def xcor(self, in1, in2):
+        """
+        Calculates the cross correlation between two 2D images.
+        The shift is expressed as the maxium value of the correlation
+        from the centre of the image.
+        
+        Parameters
+        ----------
+        in1, in2 : 2D images
+        
+        Returns
+        -------
+        shift 
+        xcor
+        """
         xcor = signal.correlate(in1, in2, mode='same')
         shift = np.argmax(xcor)-(xcor.shape[0]/2)
         return shift, xcor
 
     def get_y_shifts(self, projections):
+        """
+        Calculates the y-shifts using the cross correlation method. 
+        Sums the values along the second axis, then calculates the gradients.
+        Cross correlates the gradients.
+        
+        Parameters
+        ----------
+        projections : 3D image
+        
+        Returns
+        -------
+        shifts : in the y direction"""
         proj_sum = np.sum(projections,2)
         shifts = np.zeros(projections.shape[0])
         xcor_ar = np.zeros_like(proj_sum)
@@ -107,12 +122,36 @@ class VerticalAlignment():
         return shifts
 
     def apply_y_shifts(self, projections, trans):
+        """
+        Given a 3D image, applies the yshifts in each image as stored in trans.
+        
+        Parameters
+        ----------
+        projections : 3D image
+        trans : 1D array of shifts
+
+        Returns
+        -------
+        projections : in place
+        """
         for i in range(projections.shape[0]):
             shift = int(trans[i])
             projections[i,:,:] = np.roll(projections[i,:,:],shift, axis=0)
         return projections
 
     def projection_align_vertical(self, projections):
+        """
+        The full pipeline to perform the vertical alignment.
+        Caclulates the yshifts, applies them. Crops the result.
+        
+        Parameters
+        ----------
+        projections : 3D image
+
+        Returns
+        -------
+        projections : cropped 3D image in place
+        """
         y_trans = self.get_y_shifts(projections)
         projections = self.apply_y_shifts(projections, y_trans)
 
@@ -135,95 +174,23 @@ class VerticalAlignment():
         projections = projections[:,y_from:y_to,:]
         return projections
 
-# This function gives the shifts between projections and reprojections in terms of x and y
-def get_reprojection_shifts(projections, reprojections, sub_sample=1, sigma=[4,20]):
-    shifts = np.zeros([2, projections.shape[0]], dtype = np.float32)
-    
-    border = 20
-    
-    sig_high = sigma[1]/sub_sample
-    sig_low = sigma[0]/sub_sample
-    
-    projections = projections[:,::sub_sample,border:-border:sub_sample]
-    reprojections = reprojections[:,::sub_sample,border:-border:sub_sample]
-    
-#     projections = blur(projections, sig_low) - blur(projections, sig_high)
-#     reprojections = blur(reprojections, sig_low) - blur(reprojections, sig_high)
-    
-#     utils.plot_3_projections(projections)
-#     utils.plot_sinogram(projections)
-#     utils.plot_3_projections(reprojections)
-#     utils.plot_sinogram(reprojections)
-    
-    for i in range(projections.shape[0]):
-        image_a = projections[i]
-        image_b = reprojections[i]
 
-        if i == 0:
-            plt.figure()
-            plt.subplot(1,2,1); plt.imshow(image_a)
-            plt.subplot(1,2,2); plt.imshow(image_b)
-            plt.show()
-        
-        shift, error, diffphase = register_translation(image_a, image_b)
-        
-        # print("frame_shift:", shift)
-        
-        shifts[0,i] = shift[0]
-        shifts[1,i] = shift[1]
-    shifts *= sub_sample
-    return shifts
-
-# This function gives the shifts between projections and reprojections in terms of x and y
-def get_reprojection_shifts_com(projections, reprojections, sub_sample=1, sigma=[4,20]):
-    shifts = np.zeros([1, projections.shape[0]], dtype = np.float32)
-    
-    border = 20
-    
-    sig_high = sigma[1]/sub_sample
-    sig_low = sigma[0]/sub_sample
-    
-    projections = projections[:,::sub_sample,border:-border:sub_sample]
-    reprojections = reprojections[:,::sub_sample,border:-border:sub_sample]
-    
-    for i in range(projections.shape[0]):
-        image_a = projections[i]
-        image_b = reprojections[i]
-
-        if i == 0:
-            plt.figure()
-            plt.subplot(1,2,1); plt.imshow(image_a)
-            plt.subplot(1,2,2); plt.imshow(image_b)
-            plt.show()
-        
-        #shift, error, diffphase = register_translation(image_a, image_b)
-        
-        for i in range(projections.shape[0]):
-            a = np.sum(image_a,0)
-            b = np.sum(image_b,0)
-
-            coma = measurements.center_of_mass(a)
-            comb = measurements.center_of_mass(b)
-            
-            shifts[0,i] = coma[0]-comb[0]
-            
-        # print("frame_shift:", shift)
-        
-        #shifts[0,i] = shift[0]
-        #shifts[1,i] = shift[1]
-    #shifts *= sub_sample
-    return shifts
-
-
-
-# Alignment in the x-direction with cross-correlation
-class HorizontalAlignment(BaseClassAligment):
+class HorizontalAlignmentCrossCorrelation(BaseClassHorizontalAlignment):
     def __init__(self, projections_aligned_vert_fix2):
+        """Saves the projections as an attribute.
+        Performs the horizontal alignment with cross-correlation and saves the resulting 3D image 
+        as an attribute."""
         self.projections_aligned_vert_fix2 = projections_aligned_vert_fix2
         self.correction_rough = self.horizontal_alignment()
 
-
     def calculate_correlation(self):
+        """
+        This method uses cross correlation between images to calculate the shifts in
+        the horizontal direction. The cross correlation method is taken from "skimage".
+        
+        Returns
+        -------
+        quick_xcorrelation : the shift in the horizontal direction."""
         projections_aligned_vert_fix2 = self.projections_aligned_vert_fix2
         quick_xcorrelation = np.zeros((2,projections_aligned_vert_fix2.shape[0]))
                                     
@@ -238,10 +205,14 @@ class HorizontalAlignment(BaseClassAligment):
         plt.plot(quick_xcorrelation[1,:])
         return quick_xcorrelation
     
-    
-
-    # apply the shifts from cross-correlation to the projections
     def horizontal_alignment(self):
+        """
+        Applies the shifts calculated by "calculate_correlation" to the projections attribute.
+
+        Returns
+        -------
+        correction_rough :
+        """
         projections_aligned_vert_fix2 = self.projections_aligned_vert_fix2
         quick_xcorrelation = self.calculate_correlation()
         correction_rough = self.apply_reprojection_shifts(projections_aligned_vert_fix2, -gaussian_filter1d(quick_xcorrelation,10), pad=1)
@@ -249,17 +220,46 @@ class HorizontalAlignment(BaseClassAligment):
         plt.figure(),plt.imshow(correction_rough[:,200,:])
         return correction_rough
 
-class COMAligment(BaseClassAligment):
+
+class COMAlignment(BaseClassHorizontalAlignment):
 
     def __init__(self, projections_aligned_vert_fix, correction_rough, angles_reduced):
+        
+        """
+        Defines attributes, aligns the projections.
+
+        Parameters
+        ----------
+        projections_aligned_vert_fix :
+        correction_rough :
+        angles_reduced :
+        """
         self.projections_aligned_vert_fix = projections_aligned_vert_fix
         self.correction_rough = correction_rough
         self.angles_reduced = angles_reduced
-
         self.projections_fitted = self.alignment()
 
+    def sin_func(self, x, a, w, b, c):
+        """Fit function. Used by COMAlignment."""
+        return a * np.sin(w*x + b) + c
+
     def fit_com_to_sin(self, projections, angles, blur_filter = 2):
-    
+        """
+        Calculates the shifts by a centre of mass method. The COM is calculated using "scipy".
+        This requires a sum over one axis. Performs a fit to a sinusoidal function
+        and a gaussian filter. Applies the shifts.
+
+        Parameters 
+        ----------
+        projections :
+        angles :
+        blur_filter :
+
+        Returns
+        -------
+        projections_out
+        shifts
+        """
         projections -= np.amin(projections)
         summed = np.sum(projections[:,:,:],1)
         
@@ -273,8 +273,8 @@ class COMAligment(BaseClassAligment):
         com_range = com_max - com_min
         com_mid = com_min + com_range//2
         
-        params, params_covariance = optimize.curve_fit(sin_func, angles, com[1,:], p0=[com_range, 1, 0, com_mid])
-        ideal = sin_func(self.angles_reduced, params[0], params[1], params[2], params[3])
+        params, params_covariance = optimize.curve_fit(self.sin_func, angles, com[1,:], p0=[com_range, 1, 0, com_mid])
+        ideal = self.sin_func(self.angles_reduced, params[0], params[1], params[2], params[3])
         
         #ideal = (np.cos(angles)*com_range) + com_mid
         
---
+++ src/alignment/Alignment_work_in_progress.py
@@ -0,0 +1,118 @@
+# Author: Oriol
+# These methods are only used in the "work in progress" section of Oriol's original code.
+# -----------------------------------------------------------------------------------------
+
+import numpy as np
+import matplotlib.pyplot as plt
+import utilities.utils_tomo as utils
+from scipy.ndimage import  measurements
+from skimage.registration import phase_cross_correlation as register_translation
+
+def sin_func(x, a, w, b, c):
+    """Used by COMAlignment and work in progress."""
+    return a * np.sin(w*x + b) + c
+
+def calculate_com_projs(projections):
+    #plt.figure()
+    
+    projections -= np.amin(projections)
+    summed = np.sum(projections[:,:,:],1)
+    
+    com = np.zeros([2,projections.shape[0]])
+    shifts = np.zeros([2,projections.shape[0]])
+    for i in range(projections.shape[0]):
+        com[:,i] = measurements.center_of_mass(summed[i:i+1,:])
+    
+    return com
+
+def calculate_com_projs_oriol(projections):
+    #plt.figure()
+    
+    projections -= np.amin(projections)
+    summed = np.sum(projections[:,:,:],1)
+    
+    com = np.zeros([2,projections.shape[0]])
+    shifts = np.zeros([2,projections.shape[0]])
+    for i in range(projections.shape[0]):
+        com[:,i] = measurements.center_of_mass((summed[i:i+1,:]))
+    
+    return com
+
+def get_reprojection_shifts(projections, reprojections, sub_sample=1, sigma=[4,20]):
+    """This function gives the shifts between projections and reprojections in terms of x and y"""
+    shifts = np.zeros([2, projections.shape[0]], dtype = np.float32)
+    
+    border = 20
+    
+    sig_high = sigma[1]/sub_sample
+    sig_low = sigma[0]/sub_sample
+    
+    projections = projections[:,::sub_sample,border:-border:sub_sample]
+    reprojections = reprojections[:,::sub_sample,border:-border:sub_sample]
+    
+#     projections = blur(projections, sig_low) - blur(projections, sig_high)
+#     reprojections = blur(reprojections, sig_low) - blur(reprojections, sig_high)
+    
+#     utils.plot_3_projections(projections)
+#     utils.plot_sinogram(projections)
+#     utils.plot_3_projections(reprojections)
+#     utils.plot_sinogram(reprojections)
+    
+    for i in range(projections.shape[0]):
+        image_a = projections[i]
+        image_b = reprojections[i]
+
+        if i == 0:
+            plt.figure()
+            plt.subplot(1,2,1); plt.imshow(image_a)
+            plt.subplot(1,2,2); plt.imshow(image_b)
+            plt.show()
+        
+        shift, error, diffphase = register_translation(image_a, image_b)
+        
+        # print("frame_shift:", shift)
+        
+        shifts[0,i] = shift[0]
+        shifts[1,i] = shift[1]
+    shifts *= sub_sample
+    return shifts
+
+def get_reprojection_shifts_com(projections, reprojections, sub_sample=1, sigma=[4,20]):
+    """This function gives the shifts between projections and reprojections in terms of x and y"""
+    shifts = np.zeros([1, projections.shape[0]], dtype = np.float32)
+    
+    border = 20
+    
+    sig_high = sigma[1]/sub_sample
+    sig_low = sigma[0]/sub_sample
+    
+    projections = projections[:,::sub_sample,border:-border:sub_sample]
+    reprojections = reprojections[:,::sub_sample,border:-border:sub_sample]
+    
+    for i in range(projections.shape[0]):
+        image_a = projections[i]
+        image_b = reprojections[i]
+
+        if i == 0:
+            plt.figure()
+            plt.subplot(1,2,1); plt.imshow(image_a)
+            plt.subplot(1,2,2); plt.imshow(image_b)
+            plt.show()
+        
+        #shift, error, diffphase = register_translation(image_a, image_b)
+        
+        for i in range(projections.shape[0]):
+            a = np.sum(image_a,0)
+            b = np.sum(image_b,0)
+
+            coma = measurements.center_of_mass(a)
+            comb = measurements.center_of_mass(b)
+            
+            shifts[0,i] = coma[0]-comb[0]
+            
+        # print("frame_shift:", shift)
+        
+        #shifts[0,i] = shift[0]
+        #shifts[1,i] = shift[1]
+    #shifts *= sub_sample
+    return shifts
