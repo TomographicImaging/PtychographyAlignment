3692df88032c3d781957c4fa747b94365028cc00
Oriol Roche i Morgo <oriol.roche@diamond.ac.uk>
2025-11-04 11:48:12 +0000

    Changed user paths to cm40629-5

--- src/config/user_paths.py
+++ src/config/user_paths.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 # Your personal data directory (absolute path)
 #DATA_ROOT = Path("C:/Users/zvm34551/Coding_environment/DATA/Ptychography")
-DATA_ROOT = Path("/dls/i13-1/data/2025/cm40629-1/processing/ptycho-tomo_alignment")
+DATA_ROOT = Path("/dls/i13-1/data/2025/cm40629-5/processing/ptycho-tomo_alignment")
