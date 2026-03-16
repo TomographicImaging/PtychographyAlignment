a8b94b68470d6ef179d538acef026c834633814a
hrobarts <hannah.robarts@stfc.ac.uk>
2026-03-16 11:30:16 +0000

    move TomoRecon to validating_methods

--- src/config/user_paths.py
+++ src/config/user_paths.py
@@ -2,4 +2,5 @@ from pathlib import Path
 
 # Your personal data directory (absolute path)
 #DATA_ROOT = Path("C:/Users/zvm34551/Coding_environment/DATA/Ptychography")
-DATA_ROOT = Path("/dls/i13-1/data/2025/cm40629-5/processing/ptycho-tomo_alignment")
+# DATA_ROOT = Path("/dls/i13-1/data/2025/cm40629-5/processing/ptycho-tomo_alignment")
+DATA_ROOT = Path("/mnt/share/ALC_ptychography_alignment/")
